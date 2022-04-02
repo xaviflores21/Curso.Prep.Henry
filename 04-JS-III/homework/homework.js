e2e88e63c7c264508b02dc7d1986dbdd18ca9cd0
Javier W. Flores <willams_21@hotmail.com>
2022-04-01 20:39:40 -0400

    a

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -73,6 +73,7 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  
 }
 
 function promedioResultadosTest(resultadosTest) {
